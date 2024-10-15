a163a64df27828229000421ee94a4532a2634436
kjalba <albakevinjulian@gmail.com>
2024-10-14 22:25:54 -0500

    refactor(flag handlers): moved flag handlers to separate pkg to make main.go lightweight

--- main.go
+++ main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
 
-	"github.com/kjalba/chess-trainer-cli/chessboard"
-	"github.com/kjalba/chess-trainer-cli/game"
-	"github.com/notnil/chess"
+	"github.com/kjalba/chess-trainer-cli/puzzle"
 )
-
-type Lichess struct {
-	Game struct {
-		ID string `json:"id"`
-		PGN string `json:"pgn"`
-	} `json:"game"`
-	
-	Puzzle struct {
-		ID string `json:"id"`
-		Solution []string `json:"solution"`
-		Themes []string `json:"themes"`
-	} `json:"puzzle"`
-}
 
 var (
 	dailyPuzzle bool
@@ -50,54 +31,12 @@ func main() {
 	flag.Parse()
 	if randomPuzzle {
 		fmt.Println("Fetching a random puzzle...")
-		handleRandomPuzzle()
+		puzzle.HandleRandomPuzzle()
 	} else if dailyPuzzle {
 		fmt.Println("Fetching the daily puzzle...")
-		handleDailyPuzzle()
+		puzzle.HandleDailyPuzzle()
 	} else {
 		fmt.Println("Fetching the daily puzzle...")
-		handleDailyPuzzle()
+		puzzle.HandleDailyPuzzle()
 	}
-}
-
-func handleDailyPuzzle() {
-	res, err := http.Get("https://lichess.org/api/puzzle/daily") // daily puzzle api 
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		 log.Fatalf("Lichess API returned status code %d", res.StatusCode)
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var lichess Lichess
-	err = json.Unmarshal(body, &lichess)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	lichessGame, puzzle := lichess.Game, lichess.Puzzle
-
-	chessGame := chess.NewGame()
-	err = chessGame.UnmarshalText([]byte(lichessGame.PGN))
-	if err != nil {
-		log.Fatalf("Failed to parse PGN: %v", err)
-	}
-
-	chessGameBoard := chessGame.Position().Board()
-
-	// The notnil/chess library doesn't have a function for reversing the board's row and column headers in the drawing, so I implemented my own draw function
-	chessboard.DrawBoard(chessGameBoard, chessGame.Position().Turn() == chess.Black)
-	game.HandleUserInput(chessGame, puzzle.Solution)
-}
-
-func handleRandomPuzzle() {
-	// Logic to handle fetching random puzzles can be implemented here
-	fmt.Println("Random puzzle feature is under development.")
 }
---
+++ puzzle/handlers.go
@@ -0,0 +1,68 @@
+package puzzle
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/kjalba/chess-trainer-cli/chessboard"
+	"github.com/kjalba/chess-trainer-cli/game"
+	"github.com/notnil/chess"
+)
+
+type Lichess struct {
+	Game struct {
+		ID string `json:"id"`
+		PGN string `json:"pgn"`
+	} `json:"game"`
+	
+	Puzzle struct {
+		ID string `json:"id"`
+		Solution []string `json:"solution"`
+		Themes []string `json:"themes"`
+	} `json:"puzzle"`
+}
+
+func HandleDailyPuzzle() {
+	res, err := http.Get("https://lichess.org/api/puzzle/daily") // daily puzzle api 
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		 log.Fatalf("Lichess API returned status code %d", res.StatusCode)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var lichess Lichess
+	err = json.Unmarshal(body, &lichess)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lichessGame, puzzle := lichess.Game, lichess.Puzzle
+
+	chessGame := chess.NewGame()
+	err = chessGame.UnmarshalText([]byte(lichessGame.PGN))
+	if err != nil {
+		log.Fatalf("Failed to parse PGN: %v", err)
+	}
+
+	chessGameBoard := chessGame.Position().Board()
+
+	// The notnil/chess library doesn't have a function for reversing the board's row and column headers in the drawing, so I implemented my own draw function
+	chessboard.DrawBoard(chessGameBoard, chessGame.Position().Turn() == chess.Black)
+	game.HandleUserInput(chessGame, puzzle.Solution)
+}
+
+func HandleRandomPuzzle() {
+	// Logic to handle fetching random puzzles can be implemented here
+	fmt.Println("Random puzzle feature is under development.")
+}
